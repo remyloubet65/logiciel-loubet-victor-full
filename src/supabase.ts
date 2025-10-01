a3a6a6294588e8a24935432c9639d045e11fc79a
remyloubet65 <remyloubet65@gmail.com>
2025-10-01 15:38:24 +0200

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = import.meta.env.VITE_SUPABASE_URL as string
-const key = import.meta.env.VITE_SUPABASE_ANON_KEY as string
+// ✅ Configuration correcte Supabase (URL + clé anonyme)
+const supabaseUrl = 'https://aoidadxujanyxpxctbyvb.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvaWRhZHh1amFueHB4Y3RieXZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzMDYyNzYsImV4cCI6MjA3NDg4MjI3Nn0.Iaf6J_KzUzRNddUE6Mq6xTJW0xKc7eR6FU3OtW2CwHw'
 
-export const supabase = createClient(url, key)
+export const supabase = createClient(supabaseUrl, supabaseKey)
